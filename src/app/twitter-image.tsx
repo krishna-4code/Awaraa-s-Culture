ea5f189aadb6ffa9e8d7299652abc22e3fe447fc
krishna-4code <krishnaapis1548@gmail.com>
2026-07-26 15:20:10 +0530

    next lasest version fix

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,7 +1,5 @@
 import { ImageResponse } from 'next/og';
  
-export const runtime = 'edge';
- 
 export const alt = 'Awaraa\'s Culture - Engineered Exclusivity';
 export const size = {
   width: 1200,
